bb5caa1e9a5e629a014d29121fc977e46db78083
coendewith <coendewith@gmail.com>
2024-09-28 17:07:01 +0200

    Daily commit

--- new_crontab.sh
+++ new_crontab.sh
@@ -16,11 +16,5 @@
 
 
 
-30 9 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-33 22 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-20 15 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-39 13 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-14 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-32 14 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-15 10 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-39 16 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+22 22 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+7 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
