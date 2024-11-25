ea24f7ab1ba9e4673ddecc114bcdafa472a3b859
coendewith <coendewith@gmail.com>
2024-11-25 10:41:01 +0100

    Daily commit

--- backup_crontab.sh
+++ backup_crontab.sh
@@ -16,4 +16,6 @@
 
 
 
-37 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+3 9 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+14 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+49 13 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
