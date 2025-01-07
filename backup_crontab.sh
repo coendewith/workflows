a32b301e3535240032a678ef0f11a32046c10764
coendewith <coendewith@gmail.com>
2025-01-07 16:17:01 +0100

    Daily commit

--- backup_crontab.sh
+++ backup_crontab.sh
@@ -16,12 +16,13 @@
 
 
 
-5 13 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-23 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-21 10 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-31 22 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-43 13 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-7 10 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-26 23 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-43 9 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-47 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+5 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+50 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+21 14 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+36 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+59 16 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+17 18 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+6 15 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+15 18 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+49 21 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+56 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
--- commit_schedule.sh
+++ commit_schedule.sh
@@ -1,10 +1,2 @@
-5 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-50 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-21 14 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-36 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-59 16 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-17 18 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-6 15 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-15 18 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-49 21 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-56 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+17 16 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+22 23 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
