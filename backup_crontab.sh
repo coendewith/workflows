ebcbac38da842dc430ba116754ca0f2e13c57354
coendewith <coendewith@gmail.com>
2025-01-29 10:35:02 +0100

    Daily commit

--- backup_crontab.sh
+++ backup_crontab.sh
@@ -16,7 +16,11 @@
 
 
 
-45 10 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-48 14 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-44 23 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-55 15 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+3 9 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+5 22 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+9 10 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+49 14 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+32 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+42 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+45 22 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+8 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
--- commit_schedule.sh
+++ commit_schedule.sh
@@ -1,8 +1,3 @@
-3 9 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-5 22 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-9 10 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-49 14 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-32 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-42 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-45 22 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-8 19 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+17 16 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+35 10 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+8 12 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
