ecf2d8745083a764705d318bd7bd5017e005aa33
coendewith <coendewith@gmail.com>
2025-09-01 10:35:01 +0200

    Daily commit

--- commit_schedule.sh
+++ commit_schedule.sh
@@ -1,2 +1,7 @@
-35 9 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
-50 20 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+5 14 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+35 10 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+51 13 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+46 12 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+1 17 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+18 13 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
+9 18 * * * /usr/bin/python3 /home/pi/workflows/make_commit.py
